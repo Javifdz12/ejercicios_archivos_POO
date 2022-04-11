e1efd5d03a36ff2cc8a8fe272056f643ca1327a2
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-04-11 17:56:28 +0200

    gg

--- ejercicio.py
+++ ejercicio.py
@@ -1,8 +1,5 @@
 import pandas as pd
 
-file1 = pd.read_csv('calificaciones2.csv',sep=";")
-
-
 def informacion_alumnos(archivo):
     sep=";"
     with open(archivo) as archivo:
@@ -13,16 +10,37 @@ def informacion_alumnos(archivo):
             columnas=linea.split(sep)
             apellidos=columnas[0]
             nombre=columnas[1]
-            parcial1=columnas[2]
-            parcial2=columnas[3]
-            ordinario1=columnas[4]
-            ordinario2=columnas[5]
-            practicas=columnas[6]
-            ordinario_practicas=columnas[7]
-            alumnos.append({"Apellidos":apellidos,"Nombre":nombre,"Parcial1":parcial1,"Parcial2":parcial2,"Ordinario1":ordinario1,"Ordinario2":ordinario2,"Practicas":practicas,"OrdianrioPracticas":ordinario_practicas})
+            asistencia=columnas[2]
+            parcial1=columnas[3]
+            parcial2=columnas[4]
+            ordinario1=columnas[5]
+            ordinario2=columnas[6]
+            practicas=columnas[7]
+            ordinario_practicas=columnas[8]
+            alumnos.append({"Apellidos":apellidos,"Nombre":nombre,"Asistencia":asistencia,"Parcial1":float(parcial1),"Parcial2":float(parcial2),"Ordinario1":float(ordinario1),"Ordinario2":float(ordinario2),"Practicas":float(practicas),"OrdinarioPracticas":float(ordinario_practicas)})
         return(alumnos)
 
 print(informacion_alumnos("calificaciones2.csv"))
 
+def calificacion_final(lista):
+    for diccionario in lista:
+        nota_final=(diccionario["Parcial1"]*0.3)+(diccionario["Parcial2"]*0.3)+(diccionario["Practicas"]*0.4)
+        if diccionario["Ordinario1"]!=0 and diccionario["Ordinario2"]!=0 and diccionario["OrdinarioPracticas"]!=0:
+            nota_final=(diccionario["Ordinario1"]*0.3)+(diccionario["Ordinario2"]*0.3)+(diccionario["OrdinarioPracticas"]*0.4)
+        elif diccionario["Ordinario1"]==0 and diccionario["Ordinario2"]!=0 and diccionario["OrdinarioPracticas"]!=0:
+            nota_final=(diccionario["Parcial1"]*0.3)+(diccionario["Ordinario2"]*0.3)+(diccionario["OrdinarioPracticas"]*0.4)
+        elif diccionario["Ordinario1"]!=0 and diccionario["Ordinario2"]==0 and diccionario["OrdinarioPracticas"]!=0:
+            nota_final=(diccionario["Ordinario1"]*0.3)+(diccionario["Parcial2"]*0.3)+(diccionario["OrdinarioPracticas"]*0.4)
+        elif diccionario["Ordinario1"]!=0 and diccionario["Ordinario2"]!=0 and diccionario["OrdinarioPracticas"]==0:
+            nota_final=(diccionario["Ordinario1"]*0.3)+(diccionario["Ordinario2"]*0.3)+(diccionario["Practicas"]*0.4)
+        elif diccionario["Ordinario1"]==0 and diccionario["Ordinario2"]==0 and diccionario["OrdinarioPracticas"]!=0:
+            nota_final=(diccionario["Parcial1"]*0.3)+(diccionario["Parcial2"]*0.3)+(diccionario["OrdinarioPracticas"]*0.4)
+        elif diccionario["Ordinario1"]==0 and diccionario["Ordinario2"]!=0 and diccionario["OrdinarioPracticas"]==0:
+            nota_final=(diccionario["Parcial1"]*0.3)+(diccionario["Ordinario2"]*0.3)+(diccionario["Practicas"]*0.4)
+        elif diccionario["Ordinario1"]!=0 and diccionario["Ordinario2"]==0 and diccionario["OrdinarioPracticas"]==0:
+            nota_final=(diccionario["Ordinario1"]*0.3)+(diccionario["Parcial2"]*0.3)+(diccionario["Practicas"]*0.4)
+        diccionario["NotaFinal"]=nota_final
+    return lista
 
+print(calificacion_final(informacion_alumnos("calificaciones2.csv")))
 
