3c346cc75d5519cffde48be333fbe99fc5ed14cb
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-04-11 16:38:11 +0200

    gg

--- ejercicio.py
+++ ejercicio.py
@@ -1,11 +1,27 @@
 import pandas as pd
-import numpy as np
 
 file1 = pd.read_csv('calificaciones2.csv',sep=";")
-data=pd.DataFrame(file1,columns=file1.columns)
-print(data)
 
-def informacion(archivo):
-    pass
+
+def informacion_alumnos(archivo):
+    archivo=pd.read_csv(archivo,sep=";")
+    next(archivo)
+    alumnos=[]
+    for linea in archivo:
+        linea=linea.rstrip("/n")
+        columnas=columnas.split(";")
+        apellidos=columnas[0]
+        nombre=columnas[1]
+        parcial1=columnas[2]
+        parcial2=columnas[3]
+        ordinario1=columnas[4]
+        ordinario2=columnas[5]
+        practicas=columnas[6]
+        ordinario_practicas=columnas[7]
+        alumnos.append({"Apellidos":apellidos,"Nombre":nombre,"Parcial1":parcial1,"Parcial2":parcial2,"Ordinario1":ordinario1,"Ordinario2":ordinario2,"Practicas":practicas,"OrdianrioPracticas":ordinario_practicas})
+    return(alumnos)
+
+informacion_alumnos("calificaciones2.csv")
+
 
 
